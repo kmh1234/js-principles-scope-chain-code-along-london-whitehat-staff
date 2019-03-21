f7765eca34644de577c08c5990cb65c60dc493c1
Kelly <kelly.ho@sky.uk>
2019-03-21 12:27:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 /* Your code goes here */
+
+var customerName = 'bob';
+const leastFavouriteCustomer = 'helen';
+var bestCustomer
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+   bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer() {
+   bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavouriteCustomer = 'john';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'joe';
+  // let favoriteCustomer = 'hank';
+  throw new SyntaxError()
+}
